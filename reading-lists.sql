6c2b4ff97a4b4e0313db2b8c6e6fc261064514e5
CristinaApostu <32862328+CristinaApostu@users.noreply.github.com>
2017-12-10 23:27:04 +0200

    Update reading-lists.sql

--- reading-lists.sql
+++ reading-lists.sql
@@ -1,1 +1,40 @@
 
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+CREATE DATABASE `reading_lists`;
+USE reading_lists;
+
+--Tabela `categorie`
+
+CREATE TABLE IF NOT EXISTS `categorie`
+(
+`id` INTEGER NOT NULL AUTO_INCREMENT,
+`denumire` varchar(50) DEFAULT NULL,
+`descriere` varchar(100) DEFAULT NULL,
+`createdAt` timestamp,
+`updatedAt` timestamp,
+PRIMARY KEY(`id`),
+KEY `id` (`id`)
+)
+AUTO_INCREMENT=1;
+
+--Tabela `carte`
+
+CREATE TABLE IF NOT EXISTS `carte`
+(
+`id` INTEGER NOT NULL auto_increment,
+`id_categorie` INTEGER DEFAULT NULL,
+`titlu_carte` VARCHAR(35) DEFAULT NULL,
+`autor` VARCHAR(30) DEFAULT NULL,
+`descriere_carte` VARCHAR(100) DEFAULT NULL,
+`imagine_carte` VARCHAR(200) DEFAULT NULL,
+`createdAt` timestamp,
+`updatedAt` timestamp,
+PRIMARY KEY(`id`),
+FOREIGN KEY(`id_categorie`) REFERENCES categorie(`id`)
+)
